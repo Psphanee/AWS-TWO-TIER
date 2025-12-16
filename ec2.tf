55fefa72c4147327dd3b61fa0572d86b1bc43895
phaneeps <phaneeps@gmail.com>
2025-12-16 12:35:30 +0530

    changed ec2 type

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count           = 2
   ami             = "ami-0c02fb55956c7d316"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   subnet_id       = aws_subnet.public[count.index].id
   security_groups = [aws_security_group.web_sg.id]
 
